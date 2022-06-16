aa16d94757f2cd7e7d4d68c5501b71635dc3b0f6
Kim Ngo <kngo@nvidia.com>
2022-06-16 12:04:29 -0700

    feat(vad): Add cuda kernel preprocessor for vad pipeline

--- python/nemo2riva/nemo2riva/patches/__init__.py
+++ python/nemo2riva/nemo2riva/patches/__init__.py
@@ -3,7 +3,7 @@ from .fastpitch import generate_vocab_mapping, patch_volume
 from .mtencdec import change_tokenizer_names
 
 patches = {
-    "FastPitchModel": [generate_vocab_mapping, patch_volume],
     "EncDecCTCModelBPE": [bpe_check_inputs_and_version],
     "MTEncDecModel": [change_tokenizer_names],
+    "FastPitchModel": [generate_vocab_mapping, patch_volume],
 }
