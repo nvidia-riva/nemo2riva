552e3d8e5c83ca20040a5a4daf3efe35ffbc3012
Daniel Egert <degert@nvidia.com>
2022-09-02 10:18:05 -0700

    Added nemo2riva patches for EncDecCTCModel when decoder.num_classes is -1

--- python/nemo2riva/nemo2riva/patches/__init__.py
+++ python/nemo2riva/nemo2riva/patches/__init__.py
@@ -1,8 +1,10 @@
+from .ctc import set_decoder_num_classes
 from .ctc_bpe import bpe_check_inputs_and_version
 from .fastpitch import fastpitch_model_versioning, generate_vocab_mapping
 from .mtencdec import change_tokenizer_names
 
 patches = {
+    "EncDecCTCModel": [set_decoder_num_classes],
     "EncDecCTCModelBPE": [bpe_check_inputs_and_version],
     "MTEncDecModel": [change_tokenizer_names],
     "FastPitchModel": [generate_vocab_mapping, fastpitch_model_versioning],
---
+++ python/nemo2riva/nemo2riva/patches/ctc.py
@@ -0,0 +1,13 @@
+import yaml
+
+
+def set_decoder_num_classes(model, artifacts, **kwargs):
+    if model.__class__.__name__ == 'EncDecCTCModel':
+
+        conf = yaml.safe_load(artifacts['model_config.yaml']['content'])
+
+        if ('decoder' in conf) and ('num_classes' in conf['decoder']) and (conf['decoder']['num_classes'] == -1):
+            if 'vocabulary' in conf['decoder'] and len(conf['decoder']['vocabulary']) > 0:
+                conf['decoder']['num_classes'] = len(conf['decoder']['vocabulary'])
+
+        artifacts['model_config.yaml']['content'] = yaml.safe_dump(conf, encoding=('utf-8'))
